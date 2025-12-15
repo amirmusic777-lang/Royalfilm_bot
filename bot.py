cc012a5f634ce40853639d9e50fe7c20f20bf163
amirmusic777-lang <amirmusic777@gmail.com>
2025-12-16 00:27:32 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,22 @@
 import os
 from telegram import Update
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    ContextTypes,
-)
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ ربات RoyalFilm با موفقیت اجرا شد")
-
+    await update.message.reply_text("ربات RoyalFilm روشن شد ✅")
 
 def main():
+    if not TOKEN:
+        raise RuntimeError("BOT_TOKEN is not set")
+
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
     print("Bot started...")
     app.run_polling()
-
 
 if __name__ == "__main__":
     main()
